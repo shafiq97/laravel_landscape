623f95c36b64ec2ab36f1f7e9b3db1c49062bc9d
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-04-25 15:44:22 +0800

    latest

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -24,27 +24,34 @@
                 @endforeach
             </tbody>
         </table>
-        <h1>Pending Landscaper Reply</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>User</th>
-                    <th>Date</th>
-                    <th>Chat</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($pending_chats as $chat)
-                    @if (true)
-                        <tr>
-                            <td>{{ $chat->first_name }}</td>
-                            <td>{{ $chat->created_at->format('d/m/Y H:i') }}</td>
-                            <td><a href="{{ route('chat.landscaper', ['user_id' => $chat->user_id, 'landscaper_id' => $chat->landscaper_id, 'user_name' => $chat->first_name]) }}"
-                                    class="btn btn-warning">Chat</a></td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-        </table>
+        {{-- TODO: ONly render the table if current user role is use --}}
+        @php
+            $loggedInUser = \Illuminate\Support\Facades\Auth::user();
+            $role = $loggedInUser->userRoles->pluck('name')->toArray();
+        @endphp
+        @if ($role[0] == "User")
+            <h1>Pending Landscaper Reply</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Date</th>
+                        <th>Chat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pending_chats as $chat)
+                        @if (true)
+                            <tr>
+                                <td>{{ $chat->first_name }}</td>
+                                <td>{{ $chat->created_at->format('d/m/Y H:i') }}</td>
+                                <td><a href="{{ route('chat.landscaper', ['user_id' => $chat->user_id, 'landscaper_id' => $chat->landscaper_id, 'user_name' => $chat->first_name]) }}"
+                                        class="btn btn-warning">Chat</a></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
     </div>
 @endsection
